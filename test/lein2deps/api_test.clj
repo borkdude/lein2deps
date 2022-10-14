c726ef0c23f1146f6b2b5147c6421d0f8da7cf08
Michiel Borkent <michielborkent@gmail.com>
2022-10-14 13:21:03 +0200

    Add another eval test

--- test/lein2deps/api_test.clj
+++ test/lein2deps/api_test.clj
@@ -1,13 +1,21 @@
 (ns lein2deps.api-test
-  (:require [clojure.test :refer [deftest is] :as t]
+  (:require [clojure.test :refer [deftest is testing] :as t]
             [lein2deps.api :refer [lein2deps]]))
 
 (deftest eval-test
-  (let [deps (:deps (lein2deps {:eval true
-                               :project-clj "
+  (testing "unquote"
+    (let [deps (:deps (lein2deps {:eval true
+                                  :project-clj "
 (def version \"1.0.0\")
 
 (defproject dude/foo \"0.0.1\"
   :dependencies [[cheshire ~version]])"}))]
-    (is (= ["src"] (:paths deps)))
-    (is (= "1.0.0" (-> deps :deps (get 'cheshire/cheshire) :mvn/version)))))
+      (is (= ["src"] (:paths deps)))
+      (is (= "1.0.0" (-> deps :deps (get 'cheshire/cheshire) :mvn/version)))))
+  (testing "read-eval"
+    (let [deps (:deps (lein2deps {:eval true
+                                  :project-clj "
+(defproject dude/foo \"0.0.1\"
+  :dependencies [[cheshire #=(clojure.core/str \"1.0.\" \"0\")]])"}))]
+      (is (= ["src"] (:paths deps)))
+      (is (= "1.0.0" (-> deps :deps (get 'cheshire/cheshire) :mvn/version))))))

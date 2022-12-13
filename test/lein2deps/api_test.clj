2fa36eecdfb1184de6b7d5081343a5fe66d319f0
Jeroen van Dijk <jeroentjevandijk@gmail.com>
2022-12-13 13:39:14 +0200

    Add tests for maintaining order and printing

--- test/lein2deps/api_test.clj
+++ test/lein2deps/api_test.clj
@@ -39,3 +39,28 @@
                  [org.clojure/test.check \"1.0.13\" :scope \"provided\"]]
   :regex #\"foo\")"}))]
     (is (= "1.0.0" (-> deps :deps (get 'cheshire/cheshire) :mvn/version)))))
+
+(deftest maintain-ordered-test
+  (let [orig-deps (mapv (fn [i] (symbol "d" (str "dep" i))) (shuffle (range 10)))
+        deps-in-order (pr-str (into []
+                                    (concat (mapv (fn [dep] [dep "0.0.1"]) orig-deps)
+                                            ;; dev deps
+                                            (mapv (fn [dep] [dep "0.0.2" :scope "provided"]) orig-deps))))
+        project-clj (str "
+(defproject dude/foo \"0.0.1\"
+  :dependencies " deps-in-order ")")]
+    (let [deps (:deps (lein2deps {:project-clj project-clj}))]
+      (is (= orig-deps (-> deps :deps (keys))))
+      (is (= orig-deps (-> deps :aliases :dev :extra-deps (keys)))))
+
+    (let [deps (:deps (lein2deps {:eval true
+                                  :project-clj project-clj}))]
+      (is (= orig-deps (-> deps :deps (keys))))
+      (is (= orig-deps (-> deps :aliases :dev :extra-deps (keys)))))))
+
+(deftest prevent-namespacing-test
+  (let [output (with-out-str (lein2deps {:print true
+                                         :project-clj "
+(defproject dude/foo \"0.0.1\"
+  :dependencies [[cheshire \"1.0.0\"]])"}))]
+    (is (clojure.string/includes? output "cheshire/cheshire {:mvn/version \"1.0.0\"}"))))

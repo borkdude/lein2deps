4543408fbb69b1ee98594b91e0cc93f6d26decbe
Michiel Borkent <michielborkent@gmail.com>
2025-06-20 16:34:55 +0200

    bump

--- lein-lein2deps/project.clj
+++ lein-lein2deps/project.clj
@@ -1,11 +1,11 @@
-(defproject io.github.borkdude/lein-lein2deps "0.1.0"
+(defproject io.github.borkdude/lein-lein2deps "0.1.1"
   :description "A lein plugin to turn project.clj into deps.edn"
   :url "https://github.com/borkdude/lein2deps"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.github.borkdude/lein2deps "0.1.0"]]
+                 [io.github.borkdude/lein2deps "0.1.1"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_user
                                     :password :env/clojars_pass
